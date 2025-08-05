4263835c63a378cbaa0066208aff864b2e7d7d30
veeravelmanivannan <veeravel86@gmail.com>
2025-08-05 14:03:07 +0200

    4th  commit

--- jobsearch_helper_using_retriever.py
+++ jobsearch_helper_using_retriever.py
@@ -12,9 +12,10 @@ document = TextLoader("job_listings.txt").load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=200,chunk_overlap=10)
 chunks=text_splitter.split_documents(document)
 db=Chroma.from_documents(chunks,llm)
+retriever = db.as_retriever()
 
 text = input("Enter the text:")
-embedding_vector = llm.embed_query(text)
-docs = db.similarity_search_by_vector(embedding_vector)
+docs = retriever.invoke(text)
 
-
+for doc in docs:
+    print(doc.page_content)
